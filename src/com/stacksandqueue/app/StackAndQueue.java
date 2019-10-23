24a3373a4349483861a2550a4d6639ea11df817a
HDaniel999 <hdaniel999@gmail.com>
2019-10-23 11:40:11 -0500

    Stack excersice

--- src/com/stacksandqueue/app/StackAndQueue.java
+++ src/com/stacksandqueue/app/StackAndQueue.java
@@ -1,10 +1,35 @@
 package com.stacksandqueue.app;
 
+import java.util.Stack;
+
 public class StackAndQueue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String cad = "oAchewecodocjwqodcoijokeAjqcowjpxsalkznxijrruvgwjskazcxvhgfArtf482uioqwksladmcfnvgytp489u3qopwiAruytrhbwjqskamxc.";
+		
+		Stack<Character> letters = new Stack<Character>();
+		
+		char[] lettersArray = cad.toCharArray();
+		
+		for(Character c: lettersArray)
+		{
+			if(Character.toUpperCase(c)=='A') 
+			{
+				letters.push(c);
+			}
+		}
+		while(!letters.isEmpty())
+		{
+			System.out.println(letters.pop());
+		}
+		/*
+		for(Character c: letters)
+		{
+			System.out.println(letters.pop());
+		}
+		*/
+		
 	}
 
 }
